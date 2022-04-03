1ed9e1a913b017037e7ec92a55a408a69010e926
unknown <misterpola@gmail.com>
2022-04-03 19:07:38 +1000

    changed email destination to test in ghp

--- mail.php
+++ mail.php
@@ -3,7 +3,8 @@
   $name = $_POST['name'];
   $message = $_POST['message'];
 
-  $to = "walter@zoofilms.com.ar";
+  // $to = "walter@zoofilms.com.ar";
+  $to = "misterpola@gmail.com";
   $subject = "Consulta de " . $name . " (". $email .") desde Zoofilms.com.ar";
   $headers = "From: " . $email . "\r\n" .
   'Reply-To: '. $email;
